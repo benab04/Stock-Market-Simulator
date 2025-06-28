dcbec2fbb5c67bf07fe224c3af8dbdc37eaef9fe
benab04 <benabrahambiju@gmail.com>
2025-06-28 11:56:52 +0530

    Add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 import Providers from "@/components/Providers";
 
@@ -6,10 +7,25 @@ export const metadata = {
   description: "A real-time stock market simulation game",
 };
 
+const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
+const sourceTag = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`;
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Script
+          src={sourceTag}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GOOGLE_ANALYTICS_ID}');
+        `}
+        </Script>
         <Providers>{children}</Providers>
       </body>
     </html>
